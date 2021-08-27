1283c44bdcb0c7c5b999c538087fe4ff266a6816
elviscarlos-marques <elviscarlos.vix@hotmail.com>
2021-08-27 18:32:16 -0300

    linux

--- pages/api/nfceready.js
+++ pages/api/nfceready.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 export default async function handler(req, res) {
     if(req.method ==='POST'){
         console.log(req.method);
-        const browser = await puppeteer.launch({  headless: true,  ignoreHTTPSErrors: true });            
+        const browser = await puppeteer.launch({  headless: true,  ignoreHTTPSErrors: true, executablePath: './node_modules/puppeteer/.local-chromium/linux-901912' });            
         const page = await browser.newPage();
         let url = req.body.data;
 
